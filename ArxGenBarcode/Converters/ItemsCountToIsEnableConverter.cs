77072a12cbaedd9f250c9cf86602337584ecfcb0
HABPC\arxont <ivan.lezhnev@hotmail.com>
2018-12-10 14:02:17 +0900

    Добавил привязку отображения кнопок истории к наличию в списке значений.

---
+++ ArxGenBarcode/Converters/ItemsCountToIsEnableConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ArxGenBarcode.Converters
+{
+    public class ItemsCountToIsEnableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var boolValue = value as int?;
+            if (boolValue != null)
+            {
+                return boolValue > 0 ;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is int?)) return true;
+
+            if ((int?)value > 0)
+            {
+               return true;
+            }
+            return false;
+        }
+    }
+}
